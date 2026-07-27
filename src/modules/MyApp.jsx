de07751b73ca71853e50e6395315b761a4544eb6
saircomperu <administracion2@saircomperu.com.pe>
2026-07-27 17:42:15 -0500

    Update MyApp.jsx

--- src/modules/MyApp.jsx
+++ src/modules/MyApp.jsx
@@ -9,13 +9,13 @@ import ProtectedRoute from '../components/protectedRoute';
 import NotFound from './NotFound';
 import Loading from '../components/Loading';
 import Accessdenied from '../components/Accessdenied';
-import {Perfil} from './Usuarios/Perfil.jsx';
+import Perfil from './Usuarios/Perfil.jsx';
 
 // Vistas
 import Login from '../home/Login';
 import Inicio from '../home/Inicio';
 import Clientes from './clientes/Clientes';
-import {EquipoClientes} from './clientes/Equipos.jsx'
+import EquipoClientes from './clientes/Equipos.jsx'
 import { ClienteEquiposDetalle } from './clientes/ClienteEquiposDetalle';
 import Notificaciones from '../services/Notificaciones';
 import Usuarios from './Usuarios/Usuarios';
