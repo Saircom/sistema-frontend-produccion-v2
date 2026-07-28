74ca9a712708c6cfa56feb7e8f4eff5834a09bb2
saircomperu <administracion2@saircomperu.com.pe>
2026-07-27 19:08:05 -0500

    c

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -5,7 +5,7 @@ import { socket } from './socket.js';
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
-        'https://sistema-backend-produccion-v2-production.up.railway.app/api'
+        'http://localhost:3000/api'
 });
 
 api.interceptors.request.use(
